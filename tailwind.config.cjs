9dd48b09f446dd28e00650b7bea965d9d6c4addf
”Jakob <jako500n@stud.kea.dk>
2022-08-29 07:27:44 +0200

    5

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#E38B29",
+        "main-light": "#FDEEDC",
+        "main-medium": "#FFD8A9",
+        "accent-blight": "#60CBF7",
+        "accent-bdark": "#2082AB",
+        "accent-glight": "#786753",
+        "accent-gdark": "#78562F",
+      },
+    },
+    fontFamily: {
+      londrina1: "Londrina Shadow, cursive",
+      londrina2: "Londrina Solid, cursive",
+      radio: "Radio Canada, sans-serif",
+      rubikdirt: "Rubik Dirt, cursive",
+      rubikdis: "Rubik Distressed, cursive",
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
